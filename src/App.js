c793e84250c6eb1965b36c243abd00a4018f574c
opethdeliverance <stephen.jwalsh@yahoo.com>
2022-05-24 23:00:10 -0230

    practice

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function Header(props){
   return(
     <header>
-      <h1>{props.name}'s Kitchen</h1>
-      <p>This is me trying to use this app, I hope I do well today...</p>
+      <h1 style={{color: "blue"}}>{props.name}'s Kitchen</h1>
+      <p>This is me trying to use this app, I hope I do well today please...</p>
     </header>
     
   );
@@ -15,6 +15,14 @@ function Page(props){
   return(
     <p>{props.text} is the best way.</p>
   );
+}
+
+function Movies(props){
+  return(
+    <ul style = {{textAlign: "left"}}>
+    {props.movies.map( movie => <li>{movie}</li>)}
+    </ul>
+  )
 }
 
 function Image(props){
@@ -30,12 +38,19 @@ function Footer(props){
   )
 }
 
+const movies = [
+  "T2",
+  "Pulp Fiction",
+  "Goodfellas"
+]
+
 function App() {
   return (
     <div className="App">
       <Header name = "Raiden"/>
       <Page text = "To cook mid-rare"/>
       <Image image = "this is an image..."/>
+      <Movies movies={movies}/>
       <Footer date = {new Date().getFullYear()}/>
       
     </div>
